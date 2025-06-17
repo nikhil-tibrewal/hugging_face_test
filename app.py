5c1d568fc5e0fad0e4530a166d54a1e9c82d0c80
Nikhil Tibrewal <nikhiltibrewal@Nikhils-MacBook-Pro.local>
2025-06-17 19:10:21 +0700

    Set up initial local hugging face api project for sentiment analysis

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased")
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(input: TextInput):
+    result = classifier(input.text)
+    return result
